ccfca08557cb5ae358fdffdd3b0e0095c3a71c2b
korzkz <128216993+korzkz@users.noreply.github.com>
2023-05-01 20:28:01 +0800

    Update lol.js

--- lol.js
+++ lol.js
@@ -34,3 +34,53 @@ frame.addEventListener('mouseup', () => {
         elementss[i].removeEventListener('mouseover', color_draw)
     };
 })
+
+const button = document.getElementsByClassName('frame_deleter')[0];
+let isClicked = false;
+button.addEventListener("click", function() {
+    if (!isClicked) {
+        button.style.backgroundColor = "rgba(255, 255, 255, 0.3)"
+            //При нажатии
+        var sq = document.getElementsByClassName('sq')
+        for (var i = 0; i < sq.length; i++) {
+            sq[i].style.border = 'none';
+        }
+    } else {
+        button.style.backgroundColor = "white"
+            //При повторном нажатии
+        var sq = document.getElementsByClassName('sq')
+        for (var i = 0; i < sq.length; i++) {
+            sq[i].style.border = '2px solid rgba(255, 255, 255, 0.08)';
+        }
+    }
+    isClicked = !isClicked;
+});
+
+
+
+var border_selected = (element) => {
+    element.target.style.borderColor = 'white'
+}
+
+var border_selected_out = (elemen) => {
+    elemen.target.style.borderColor = ''
+
+}
+
+frame.addEventListener('mouseover', () => {
+    var elementss = document.getElementsByClassName('sq')
+    for (var i = 0; i < elementss.length; i++) {
+        elementss[i].addEventListener('mouseover', border_selected)
+    };
+})
+
+
+
+
+
+frame.addEventListener('mouseout', () => {
+    var elementsss = document.getElementsByClassName('sq')
+    for (var i = 0; i < elementsss.length; i++) {
+        elementsss[i].addEventListener('mouseout', border_selected_out)
+    };
+})
